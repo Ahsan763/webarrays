209f61b1ba62c68cca4499934a1ef6c0c2ecb20f
Ahsan763 <naqashahsan@gmail.com>
2024-08-12 16:37:30 +0500

    aa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,67 @@
-$(document).ready(function () {
-  $(".toggle-menu").click(function () {
-$(this).addClass("dnone");
-$(".body-aside").addClass("active");
-$(".navlist").addClass("active");
-$(".asid-bg").addClass("active");
-$(".nav-logo").addClass("active");
-$(".close-btn").removeClass("dnone").addClass("active");
-$(".mobile-nav").addClass("position_fixed");
-
-
+AOS.init({
+  offset: 170,
+  delay: 100,
+  duration: 1200,
+  easing: 'ease',
 });
 
-$(".close-btn").click(function () {
-$(this).addClass("dnone").removeClass("active");
-$(".body-aside").removeClass("active");
-$(".navlist").removeClass("active");
-$(".asid-bg").removeClass("active");
-$(".nav-logo").removeClass("active");
-$(".toggle-menu").removeClass("dnone");
-$(".mobile-nav").removeClass("position_fixed");
+var tl1 = gsap.timeline({defaults:{ease: Power1.easeIn,}});
+tl1.to(".hero__hdng-sp",{y:'0%',duration:0.7,stagger:0.2});
+// tl1end
+var tl2 = gsap.timeline({scrollTrigger:{
+  trigger:".banner",
+  // markers:true,
+  start:"70% 90%",
+  end:"150% 90%",
+  scrub:2,
+  pin:true
+}});
+tl2.fromTo(".banner__img",{opacity:0,scaleY:0},{opacity:1,duration:0.9,scaleY:1,ease: "slowMo.easeIn"},)
+// tl2end
 
-});
-$(".togle_nav_link").click(function () {
-$(".close-btn").addClass("dnone").removeClass("active");
-$(".body-aside").removeClass("active");
-$(".navlist").removeClass("active");
-$(".asid-bg").removeClass("active");
-$(".nav-logo").removeClass("active");
-$(".toggle-menu").removeClass("dnone");
-$(".mobile-nav").removeClass("position_fixed");
-})
-})
-$(document).ready(function() {
-    // communities section box slide animation
-    $('.box').click(function() {
-        $('p').slideToggle();
-    })
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 70) {
-            $('.nav').addClass('affix');
-        } else {
-            $('.nav').removeClass('affix');
-        }
-    });
-    // navbar sticky end
-    $('#bar-icon').click(function(){
-        $('.main-menu').toggleClass('show');
-    });
-    //   $(".black-overlay").toggleClass('active');
-       // overlay
-       $("#bar-icon").click(function(){
-        $(".black-overlay").toggleClass("d-none");
-      });
-    //   
-    $(".black-overlay").click(function(){
-    $("ul").removeClass("show");
-    $(this).addClass('d-none');
-    });  
-      //   
-    $(".black-overlay").click(function(){
-    $("ul").removeClass("show");
-    $(this).addClass('d-none');
-    });
-      //   
-      $(".body-wrapper").click(function(){
-        $(".sub-menu-list").addClass("d-none");
 
-        }); 
-	// navbar end
+
+
+
+// tl3end
+
+
+
+
+
+
+
+var tl = gsap.timeline({scrollTrigger:{
+  trigger:".contact",
+  // markers:true,
+  start:"50% 50%",
+  end:"bottom 20%",
+  scrub:2,
+  pin:true
+}});
+tl
+.to("#center",{
+ height: "100vh",
+},'a')
+.to("#top",{
+  top: "-50%",
+},'a')
+.to("#bottom",{
+  bottom: "-50%",
+},'a')
+.to("#top-h1",{
+  bottom: "-160px"
+},'a')
+.to("#bottom-h1",{
+  top: "-160px"
+},'a')
+.to(".contentform",{
+ top: "0px"
+},'a')
+.to(".content",{
+ delay: -0.2,
+ top: "0px"
 });
+
+
+
